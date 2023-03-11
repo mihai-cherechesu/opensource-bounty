6b6cfc2f07d7976dfa83e28faccc164da3ff7d42
Mihai Cherechesu <mihai.cherechesu@gmail.com>
2023-03-11 13:20:04 +0200

    Add SM implem
    Signed-off-by: Mihai Cherechesu <mihai.cherechesu@gmail.com>

---
+++ sc/wasm/src/lib.rs
@@ -0,0 +1,22 @@
+////////////////////////////////////////////////////
+////////////////// AUTO-GENERATED //////////////////
+////////////////////////////////////////////////////
+
+#![no_std]
+
+elrond_wasm_node::wasm_endpoints! {
+    contribute
+    (
+        claim_bounty
+        claim_fund
+        fund_issuer
+        getHunterWallets
+        getIssuebounties
+        getIssuerFunds
+        getIssuerWallets
+        register_hunter
+        register_issue
+    )
+}
+
+elrond_wasm_node::wasm_empty_callback! {}
